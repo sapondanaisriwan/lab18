a32bdfdf85f311d435d35dd70f3619f0e30020f8
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2025-02-10 22:18:50 +0700

    lab1 completed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,11 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R1, Rect R2) {
+    double x_overlap = min(R1.x + R1.w, R2.x + R2.w) - max(R1.x, R2.x);
+    double y_overlap = min(R1.y, R2.y) - max(R1.y - R1.h, R2.y - R2.h);
+    if (x_overlap <= 0 || y_overlap <=0) {
+        return 0;
+    }
+    return x_overlap * y_overlap;
 }
